32e0d6c69137d4265d1d8c3b7cd6b251540d9c18
Salahuddin Hairai <mr.od3n@gmail.com>
2024-03-05 11:09:11 +0800

    Fetching data

--- src/react-query/TodoList.tsx
+++ src/react-query/TodoList.tsx
@@ -1,35 +1,36 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 interface Todo {
-  id: number;
-  title: string;
-  userId: number;
-  completed: boolean;
+  id: number
+  title: string
+  userId: number
+  completed: boolean
 }
 
 const TodoList = () => {
-  const [todos, setTodos] = useState<Todo[]>([]);
-  const [error, setError] = useState('');
-
-  useEffect(() => {
+  const fetchTodos = () =>
     axios
-      .get('https://jsonplaceholder.typicode.com/todos')
-      .then((res) => setTodos(res.data))
-      .catch((error) => setError(error));
-  }, []);
+      .get<Todo[]>('https://jsonplaceholder.typicode.com/todos')
+      .then((res) => res.data)
 
-  if (error) return <p>{error}</p>;
+  const { data: todos } = useQuery({
+    queryKey: ['todos'],
+    queryFn: fetchTodos,
+  })
+
+  // if (error) return <p>{error}</p>
 
   return (
-    <ul className="list-group">
-      {todos.map((todo) => (
-        <li key={todo.id} className="list-group-item">
+    <ul className='list-group'>
+      {todos?.map((todo) => (
+        <li key={todo.id} className='list-group-item'>
           {todo.title}
         </li>
       ))}
     </ul>
-  );
-};
+  )
+}
 
-export default TodoList;
+export default TodoList
