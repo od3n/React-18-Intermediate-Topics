cc7c4810b6da7ed4291e0b82e853c40d15dc225c
Salahuddin Hairai <mr.od3n@gmail.com>
2024-03-05 17:29:40 +0800

    Exercise: Fetching data

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import './App.css'
-import TodoList from './react-query/TodoList'
+import './App.css';
+import PostList from './react-query/PostList';
 
 function App() {
-  return <TodoList />
+  return <PostList />;
 }
 
-export default App
+export default App;
