d1e975e23e9d7d59cd0335e1e389a9766bf82df8
Salahuddin Hairai <mr.od3n@gmail.com>
2024-03-05 17:19:49 +0800

    Customizing query settings

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,18 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 
 const queryClient = new QueryClient();
+// {
+//   defaultOptions: {
+//     queries: {
+//       retry: 3,
+//       cacheTime: 300_000, // 5 minutes
+//       staleTime: 10 * 1000, // 10 seconds
+//       refetchOnWindowFocus: false,
+//       refetchOnReconnect: false,
+//       refetchOnMount: false,
+//     },
+//   },
+// }
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
