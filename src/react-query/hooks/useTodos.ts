4a923142223cbab4f28116ee01bd8f29b3e7e0da
Salahuddin Hairai <mr.od3n@gmail.com>
2024-03-17 23:55:18 +0800

    Creating a custom mutation hook

--- src/react-query/hooks/useTodos.ts
+++ src/react-query/hooks/useTodos.ts
@@ -1,24 +1,25 @@
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
+import { CACHE_KEY_TODOS } from '../constants';
 
 export interface Todo {
-  id: number
-  title: string
-  userId: number
-  completed: boolean
+  id: number;
+  title: string;
+  userId: number;
+  completed: boolean;
 }
 
 const useTodos = () => {
   const fetchTodos = () =>
-  axios
-    .get<Todo[]>('https://jsonplaceholder.typicode.com/todos')
-    .then((res) => res.data)
+    axios
+      .get<Todo[]>('https://jsonplaceholder.typicode.com/todos')
+      .then((res) => res.data);
 
   return useQuery<Todo[], Error>({
-    queryKey: ['todos'],
+    queryKey: CACHE_KEY_TODOS,
     queryFn: fetchTodos,
-    staleTime: 10 * 1000
-  })
-}
+    staleTime: 10 * 1000,
+  });
+};
 
 export default useTodos;
